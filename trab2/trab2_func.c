779e313049860067ca103669e4566858c6765ad9
GabrielMarquezan <165740954+GabrielMarquezan@users.noreply.github.com>
2024-10-04 15:21:27 -0300

    Update trab2_func.c

--- trab2/trab2_func.c
+++ trab2/trab2_func.c
@@ -13,7 +13,7 @@ void le_dimensoes_matriz(Matriz* matriz)
 char** aloca_matriz(int linhas, int colunas)
 {
     //Aloca as linhas da matriz dinamicamente e testa se houve êxito
-    char** matriz = malloc(linhas * sizeof(char*));
+    char** matriz = (char**) malloc(linhas * sizeof(char*));
     if(matriz == NULL)
     {
         printf("\n\nErro 1 na função 'aloca_matriz'\n\n");
@@ -24,7 +24,7 @@ char** aloca_matriz(int linhas, int colunas)
     {
         //Aloca as colunas da matriz dinamicamente e testa se houve êxito
         //OBS: (colunas + 1) serve para garantir o espaço do '\0' no fim da linha
-        matriz[i] = malloc((colunas + 1) * sizeof(char));
+        matriz[i] = (char*) malloc((colunas + 1) * sizeof(char));
         if(matriz[i] == NULL)
         {
             printf("\n\nErro 2 na função 'aloca_matriz'\n'i' referente ao erro: %d\n\n", i);
@@ -41,19 +41,6 @@ void inicializa_matriz(Matriz* matriz)
     {
         for(int j = 0; j < matriz->numero_de_colunas; j++) matriz->matriz[i][j] = ' ';
     }
-}
-
-char* aloca_vetor_para_guardar_string(int tamanho_do_texto_para_matriz)
-{
-    //Aloca o vetor dinamicamente e testa se houve êxito
-    char *texto_para_matriz = malloc(tamanho_do_texto_para_matriz * sizeof(char));
-    if(texto_para_matriz == NULL)
-    {
-        printf("\n\nErro 1 na função 'aloca_vetor_para_guardar_string'\n\n");
-        exit(1);
-    }
-
-    return texto_para_matriz;
 }
 
 void le_texto_para_matriz(char texto_para_matriz[], int tamanho_do_texto_para_matriz)
@@ -93,17 +80,11 @@ void formatar_string_para_matriz(char texto_para_matriz[], int tamanho_do_texto_
     coloca_tudo_em_minusculo(texto_para_matriz, tamanho_do_texto_para_matriz);
 }
 
-char* texto_pronto_para_matriz(int linhas, int colunas)
+void texto_pronto_para_matriz(char texto_para_matriz[], int tamanho_do_texto_para_matriz)
 {
-    /*tamanho_do_texto_para_matriz recebe um valor arbitrariamente maior que o alocado para garantir que 
-    nada seja truncado pela existência de espaços na entrada do usuário*/
-    int tamanho_do_texto_para_matriz = (linhas * colunas * 1.5);
-    char* texto_para_matriz = aloca_vetor_para_guardar_string(tamanho_do_texto_para_matriz);
-
     le_texto_para_matriz(texto_para_matriz,tamanho_do_texto_para_matriz);
     formatar_string_para_matriz(texto_para_matriz, tamanho_do_texto_para_matriz);
-
-    return texto_para_matriz;
+    texto_para_matriz[tamanho_do_texto_para_matriz - 1] = '\0';
 }
 
 void insere_barra_zeros(Matriz* matriz)
@@ -114,7 +95,12 @@ void insere_barra_zeros(Matriz* matriz)
 
 void preenche_matriz(Matriz* matriz)
 {   
-    char* texto_para_matriz = texto_pronto_para_matriz(matriz->numero_de_linhas, matriz->numero_de_colunas);
+    /*texto para matriz tem um tamanho arbitrariamente maior que o alocado para garantir que 
+    nada seja truncado pela existência de espaços na entrada do usuário*/
+    int tamanho_do_texto_para_matriz = matriz->numero_de_linhas * matriz->numero_de_colunas * 1.5;
+    char texto_para_matriz[tamanho_do_texto_para_matriz];
+
+    texto_pronto_para_matriz(texto_para_matriz, tamanho_do_texto_para_matriz);
     int contador = 0;
 
     //Preenche a matriz com a entrada de dados devidamente tratada
@@ -134,34 +120,38 @@ void preenche_matriz(Matriz* matriz)
     insere_barra_zeros(matriz);
 }
 
-void imprime_matriz(Matriz* matriz)
+void imprime_matriz(char** matriz, int linhas, int colunas)
 {
-    printf("\n   ");
+    printf("\n    ");
 
     //Imprime o índice das colunas
-    for(int i = 0; i < matriz->numero_de_colunas; i++)
+    for(int i = 0; i < colunas; i++)
     {
         if(i < 9) 
         {
-            printf("| %d ", i + 1);
+            printf("| 0%d ", i + 1);
         }
-        else printf("| %d", i + 1);
+        else printf("| %d ", i + 1);
     }
 
     printf("|\n");
 
-    for(int i = 0; i < matriz->numero_de_linhas; i++)
+    for(int i = 0; i < linhas; i++)
     {
         //Divide as linhas
-        for(int i = 0; i < 4 * matriz->numero_de_colunas + 4; i++) printf("-");
+        for(int i = 0; i < 5 * colunas + 5; i++) printf("-");
         
         //Imprime o índice das linhas
-        printf("\n %d ", i + 1);
+        if(i < 9)
+        {
+           printf("\n 0%d ", i + 1); 
+        } 
+        else printf("\n %d ", i + 1);
 
         //Imprime o conteúdo da matriz
-        for(int j = 0; j < matriz->numero_de_colunas; j++)
+        for(int j = 0; j < colunas; j++)
         {
-            if(matriz->matriz[i][j] >= 97 || matriz->matriz[i][j] <= 122) printf("| %c ", matriz->matriz[i][j]);
+            if(matriz[i][j] >= 97 || matriz[i][j] <= 122) printf("|  %c ", matriz[i][j]);
         }
 
         printf("|\n");
@@ -170,10 +160,20 @@ void imprime_matriz(Matriz* matriz)
     printf("\n");
 }
 
-void desaloca_matriz(Matriz* matriz)
+void desaloca_matriz_e_transposta(Matriz* matriz)
 {
     for(int i = 0; i < matriz->numero_de_linhas; i++) free(matriz->matriz[i]);
     free(matriz->matriz);
+
+    for(int i = 0; i < matriz->numero_de_colunas; i++) free(matriz->transposta[i]);
+    free(matriz->transposta);
+}
+
+void le_palavra_buscada(char palavra_buscada[], Matriz matriz)
+{
+    printf("\nInsira a palavra a ser procurada: ");
+    fgets(palavra_buscada, matriz.numero_de_colunas, stdin);
+    palavra_buscada[strlen(palavra_buscada) - 1] = '\0';
 }
 
 int seleciona_sentido_da_busca(void)
@@ -187,12 +187,14 @@ int seleciona_sentido_da_busca(void)
         printf("2 - Horizontal inversa\n");
         printf("3 - Vertical direta\n");
         printf("4 - Vertical inversa\n");
-        printf("5 - Diagonal direta\n");
-        printf("6 - Diagonal inversa\n");
+        printf("5 - Diagonal principal direta\n");
+        printf("6 - Diagonal principal inversa\n");
+        printf("7 - Diagonal secundária direta\n");
+        printf("8 - Diagonal secundária inversa\n");
         printf("Insira o número de sua opção: ");
         scanf("%d", &opcao_de_busca);
     }
-    while(opcao_de_busca < 1 || opcao_de_busca > 6);
+    while(opcao_de_busca < 1 || opcao_de_busca > 8);
 
     return opcao_de_busca;
 }
@@ -200,94 +202,128 @@ int seleciona_sentido_da_busca(void)
 void inverte_string(char string[], int tamanho)
 {
     char auxiliar = ' ';
-    int indice_inverso = tamanho;
+    int indice_inverso = tamanho - 1;
 
     for(int i = 0; i < tamanho / 2; i++)
     {
         auxiliar = string[i];
-        string[i] = string[indice_inverso - 1];
-        string[indice_inverso - 1] = auxiliar;
+        string[i] = string[indice_inverso];
+        string[indice_inverso] = auxiliar;
         indice_inverso--;
     }
 }
 
-void calcula_fim_da_palavra(Palavra* palavra_buscada, int tipo_de_busca)
+void calcula_fim_da_palavra(Coordenadas* posicoes, char palavra_buscada[], int tipo_de_busca)
 {
     switch(tipo_de_busca)
     {
         case 1:
-            palavra_buscada->fim.x = palavra_buscada->inicio.x + strlen(palavra_buscada->palavra) - 1;
-            palavra_buscada->fim.y = palavra_buscada->inicio.y;
+            posicoes->fim.x = posicoes->inicio.x + strlen(palavra_buscada) - 1;
+            posicoes->fim.y = posicoes->inicio.y;
             break;
         case 2:
-            palavra_buscada->fim.x = palavra_buscada->inicio.x - strlen(palavra_buscada->palavra) + 1;
-            palavra_buscada->fim.y = palavra_buscada->inicio.y;
+            posicoes->fim.x = posicoes->inicio.x - strlen(palavra_buscada) + 1;
+            posicoes->fim.y = posicoes->inicio.y;
+            break;
+        case 3:
+            posicoes->fim.x = posicoes->inicio.x;
+            posicoes->fim.y = posicoes->inicio.y + strlen(palavra_buscada) - 1;
+            break;
+        case 4:
+            posicoes->inicio.y += strlen(palavra_buscada) - 1; 
+            posicoes->fim.x = posicoes->inicio.x;
+            posicoes->fim.y = posicoes->inicio.y - strlen(palavra_buscada) + 1;
             break;
     }
 }
 
-void atualiza_posicao_da_palavra(Matriz* matriz, char* endereco_base, Palavra* palavra_buscada, int tipo_de_busca)
-{
-    for(int i = 0; i < matriz->numero_de_linhas; i++)
+void atualiza_posicao_da_palavra(Matriz* matriz, char* endereco_base, Coordenadas* posicoes, char palavra_buscada[], int tipo_de_busca)
+{   
+    bool pesquisar_na_transposta = false;
+
+    if(tipo_de_busca == 3 || tipo_de_busca == 4) pesquisar_na_transposta = true;
+
+    if(pesquisar_na_transposta)
     {
-        for(int j = 0; j < matriz->numero_de_colunas; j++)
+        for(int i = 0; i < matriz->numero_de_colunas; i++)
         {
-            if(endereco_base == &matriz->matriz[i][j])
+            for(int j = 0; j < matriz->numero_de_linhas; j++)
             {
-                palavra_buscada->inicio.x = j + 1;
-                palavra_buscada->inicio.y = i + 1;
-                calcula_fim_da_palavra(palavra_buscada, tipo_de_busca);
+                if(endereco_base == &matriz->transposta[i][j])
+                {
+                    posicoes->inicio.x = i + 1;
+                    posicoes->inicio.y = j + +1;
+                    calcula_fim_da_palavra(posicoes, palavra_buscada, tipo_de_busca);
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int i = 0; i < matriz->numero_de_linhas; i++)
+        {
+            for(int j = 0; j < matriz->numero_de_colunas; j++)
+            {
+                if(endereco_base == &matriz->matriz[i][j])
+                {
+                    posicoes->inicio.x = j + 1;
+                    posicoes->inicio.y = i + 1;
+                    calcula_fim_da_palavra(posicoes, palavra_buscada, tipo_de_busca);
+                }
             }
         }
     }
 }
 
-char* busca_palavra_horizontal_direta(Matriz* matriz, Palavra* palavra_buscada)
+void transpoe_matriz(Matriz* matriz)
 {
-    char* primeiro_endereco_palavra = NULL;
-
     for(int i = 0; i < matriz->numero_de_linhas; i++)
     {
-        primeiro_endereco_palavra = strstr(matriz->matriz[i], palavra_buscada->palavra);
-        if(primeiro_endereco_palavra != NULL) return primeiro_endereco_palavra;
+        for(int j = 0; j < matriz->numero_de_colunas; j++) matriz->transposta[j][i] = matriz->matriz[i][j];
     }
-
-    return primeiro_endereco_palavra;
 }
 
-char* busca_palavra_horizontal_inversa(Matriz* matriz, Palavra* palavra_buscada)
-{
-    char* primeiro_endereco_palavra = NULL;
-
-    //Inverte a palavra buscada 
-    inverte_string(palavra_buscada->palavra, strlen(palavra_buscada->palavra));
-
-    for(int i = 0; i < matriz->numero_de_linhas; i++)
-    {
-        primeiro_endereco_palavra = strstr(matriz->matriz[i], palavra_buscada->palavra);
-        if(primeiro_endereco_palavra != NULL) return primeiro_endereco_palavra;
-    }
-
-    return primeiro_endereco_palavra;
-}
-
-void busca_palavra(Matriz* matriz, Palavra* palavra_buscada)
+void busca_palavra(Matriz* matriz, Coordenadas* posicoes, char palavra_buscada[])
 {
     int opcao_de_busca = seleciona_sentido_da_busca();
     char* primeiro_endereco_palavra = NULL;
+    bool pesquisar_na_transposta = false;
 
     switch(opcao_de_busca)
     {
         case 1:
-            primeiro_endereco_palavra = busca_palavra_horizontal_direta(matriz, palavra_buscada);
             break;
         case 2:
-            primeiro_endereco_palavra = busca_palavra_horizontal_inversa(matriz, palavra_buscada);
+            inverte_string(palavra_buscada, strlen(palavra_buscada));
+            break;
+        case 3:
+            pesquisar_na_transposta = true;
+            break;
+        case 4:
+            pesquisar_na_transposta = true;
+            inverte_string(palavra_buscada, strlen(palavra_buscada));
             break;
     }
 
+    if(pesquisar_na_transposta)
+    {
+        for(int i = 0; i < matriz->numero_de_colunas; i++)
+        {
+            primeiro_endereco_palavra = strstr(matriz->transposta[i], palavra_buscada);
+            if(primeiro_endereco_palavra != NULL) break;
+        }
+    }
+    else
+    {
+        for(int i = 0; i < matriz->numero_de_linhas; i++)
+        {
+            primeiro_endereco_palavra = strstr(matriz->matriz[i], palavra_buscada);
+            if(primeiro_endereco_palavra != NULL) break;
+        }
+    }
+    
     if(primeiro_endereco_palavra != NULL) 
     {
-        atualiza_posicao_da_palavra(matriz, primeiro_endereco_palavra, palavra_buscada, opcao_de_busca);
+        atualiza_posicao_da_palavra(matriz, primeiro_endereco_palavra, posicoes, palavra_buscada, opcao_de_busca);
     }
 }
