3ab8cc26940a61cf691c0e2bd827e1fdac417d5c
GabrielMarquezan <165740954+GabrielMarquezan@users.noreply.github.com>
2024-11-04 19:23:15 -0300

    Fix: adicionei a opção de exclusão ao menu e dei opção de parar a busca ao selecionar relatorio de projeto

--- trab3/menu.c
+++ trab3/menu.c
@@ -6,11 +6,12 @@ void exibe_menu() {
     printf("2. Criar novo professor\n");
     printf("3. Criar novo projeto\n");
     printf("4. Criar novo vínculo\n");
-    printf("5. Listar alunos\n");
-    printf("6. Listar professores\n");
-    printf("7. Listar projetos\n");
-    printf("8. Listar vínculos\n");
-    printf("9. Imprimir relatório de projeto\n");
+    printf("5. Excluir vínculo\n");
+    printf("6. Listar alunos\n");
+    printf("7. Listar professores\n");
+    printf("8. Listar projetos\n");
+    printf("9. Listar vínculos\n");
+    printf("10. Imprimir relatório de projeto\n");
     printf("0. Sair\n");
     printf("Escolha uma opção: ");
 }
@@ -44,31 +45,35 @@ int processa_escolha(lista** lista_alunos, lista** lista_professores, lista** li
             *lista_vinculos = (lista*) insere_no_inicio_da_lista(vinculo_novo, *lista_vinculos);
             break;
         case 5:
-            imprime_lista(*lista_alunos, ALUNOS);
+            *lista_vinculos = exclui_vinculo(*lista_vinculos, *lista_alunos, *lista_projetos);
             break;
         case 6:
-            imprime_lista(*lista_professores, PROFESSORES);
+            imprime_lista(*lista_alunos, ALUNOS);
             break;
         case 7:
-            imprime_lista(*lista_projetos, PROJETOS);
+            imprime_lista(*lista_professores, PROFESSORES);
             break;
         case 8:
+            imprime_lista(*lista_projetos, PROJETOS);
+            break;
+        case 9:
             imprime_lista(*lista_vinculos, VINCULOS);
             break;
-        case 9: {
+        case 10: 
             int codigo_do_projeto;
             projetos* projeto = NULL;
             do
             {
-                printf("Digite o código do projeto: ");
+                printf("Digite o código do projeto (digite -1 para parar a busca): ");
                 scanf("%d", &codigo_do_projeto);
+                if(codigo_do_projeto == -1) break;
                 projeto = acha_na_lista(*lista_projetos, codigo_do_projeto, PROJETOS, false);
                 if(projeto == NULL) printf("Projeto não encontrado!\n");
             } while (projeto == NULL);
+            if(codigo_do_projeto == -1) break;
             vinculos* vinculo = acha_na_lista(*lista_vinculos, codigo_do_projeto, VINCULOS, false);
             imprime_relatorio_projeto(projeto, vinculo);
             break;
-        }
         default:
             printf("Opção inválida! Tente novamente.\n");
     }
