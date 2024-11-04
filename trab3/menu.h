6bd9dcfb3d5a5e75855beb2b149e4ddb9976dfd0
GabrielMarquezan <165740954+GabrielMarquezan@users.noreply.github.com>
2024-11-04 18:50:11 -0300

    Feat: implementa menu e funções de processamento de escolhas

---
+++ trab3/menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "bolsistas.h"
+
+void exibe_menu();
+int processa_escolha(lista** lista_alunos, lista** lista_professores, lista** lista_projetos, lista** lista_vinculos);
+
+#endif
