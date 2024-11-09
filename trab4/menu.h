07764bd10b33c40cf9a620cf370e02f8e09875e9
GabrielMarquezan <165740954+GabrielMarquezan@users.noreply.github.com>
2024-11-09 16:10:31 -0300

    Feat: adicionei função de retirar pratos sem desocupar mesas e fixei vários bugs

--- trab4/menu.h
+++ trab4/menu.h
@@ -4,6 +4,6 @@
 #include "mesas.h"
 
 void imprime_menu();
-int processa_escolha(Mesa** mesas, Pilha_de_Pratos** pilha, Fila_de_Espera** fila, int linhas, int colunas, bool* restaurante_aberto);
+int processa_escolha(Mesa*** mesas, Pilha_de_Pratos** pilha, Fila_de_Espera** fila, int* linhas, int* colunas, bool* restaurante_aberto);
 
 #endif
