7cfd612bdd759238e4e52334e4c837e9ccfff5c7
GabrielMarquezan <165740954+GabrielMarquezan@users.noreply.github.com>
2024-11-09 16:09:20 -0300

    Feat: adicionei função de retirar pratos sem desocupar mesas e fixei vários bugs

--- trab4/menu.c
+++ trab4/menu.c
@@ -1,79 +1,78 @@
 #include "menu.h"
 
-/*1) Abrir restaurante: informar quantidade de mesas (linhas e colunas da matriz) e arrumar as mesas completas.
-2) Chegar (grupo de) clientes ao restaurante: implica em ocupar mesa se há disponibilidade ou ir pra fila de espera.
-3) Finalizar refeição/liberar mesa: sempre que uma mesa é liberada, a mesa completa é arrumada (4 pratos), clientes
-são chamados da fila de espera (se houver) e a quantidade de pratos é ajustada na mesa (tirar pratos excedentes da
-mesa, se necessário, e recolocá-los na pilha).
-4) Desistir de esperar: sair da fila de espera.
-5) Repor pratos: adicionar pratos na pilha.
-6) Imprimir pilha de pratos, fila de espera e ocupação das mesas, conforme descrito a seguir:
- Ocupação das mesas (número da mesa e quantidade de pessoas que ocupam a mesa) - o usuário pode pesquisar
-por número de mesa ou então consultar todas as mesas;
- Fila de espera (quantos grupos estão na fila de espera, e quantas pessoas aguardam na fila de espera. Por ex:
-grupo 1 aguarda por 3 lugares, grupo 2 aguarda por 5 lugares, logo há 2 grupos aguardando, e um total de 8 pessoas
-esperam na fila);
- Pilha de pratos (quantos pratos existem na pilha de pratos).*/
-
 void imprime_menu()
 {
-    printf("Escolha uma opção:\n");
+    printf("\t\tMENU\n");
     printf("1 - Abrir restaurante\n");
-    printf("2 - Chegar (grupo de) clientes ao restaurante\n");
+    printf("2 - Clientes chegaram!\n");
     printf("3 - Finalizar refeição/liberar mesa\n");
-    printf("4 - Desistir de esperar\n");
-    printf("5 - Imprimir pilha de pratos\n");
-    printf("6 - Imprimir fila de espera\n");
-    printf("7 - Imprimir ocupação das mesas\n");
-    printf("8 - Sair\n");
+    printf("4 - Retirar pratos da mesa (não libera mesa)\n");
+    printf("5 - Desistir de esperar\n");
+    printf("6 - Imprimir pilha de pratos\n");
+    printf("7 - Imprimir fila de espera\n");
+    printf("8 - Imprimir ocupação das mesas\n");
+    printf("9 - Sair\n");
+    printf("Escolha uma opção: ");
     
 }
 
-int processa_escolha(Mesa** mesas, Pilha_de_Pratos** pilha, Fila_de_Espera** fila, int linhas, int colunas, bool* restaurante_aberto)
+int processa_escolha(Mesa*** mesas, Pilha_de_Pratos** pilha, Fila_de_Espera** fila, int* linhas, int* colunas, bool* restaurante_aberto)
 {
     int escolha = 0;
+    int id_mesa = -2;
 
     scanf("%d", &escolha);
-    if(escolha < 1 || escolha > 8)
+    if(escolha < 1 || escolha > 9)
     {
-        printf("Escolha inválida. Tente novamente.\n");
+        printf("Escolha inválida. Tente novamente.\n\n");
         return 1;
     }
 
     switch (escolha)
     {
     case 1:
-        inicializar_restaurante(&mesas, pilha, restaurante_aberto);
+        inicializar_restaurante(mesas, pilha, linhas, colunas,  restaurante_aberto);
         break;
     case 2:
         adiciona_grupo_na_fila(fila);
+        ocupa_mesa(fila, *mesas, pilha, *linhas, *colunas);
         break;
     case 3:
-        int id_mesa = -2;
-
-        while(id_mesa < 1 || id_mesa > linhas * colunas)
+        while(id_mesa < 1 || id_mesa > (*linhas) * (*colunas))
         {
             printf("Informe o número da mesa que deseja liberar (ou -1 para cancelar): ");
             scanf("%d", &id_mesa);
 
-            if(id_mesa == -1) break;
+            if(id_mesa == -1) return 1;
         }
-        libera_mesa(fila, pilha, mesas, linhas, colunas, id_mesa);
-        ocupa_mesa(fila, mesas, pilha, id_mesa, linhas, colunas);
+        libera_mesa(fila, pilha, *mesas, *linhas, *colunas, id_mesa);
+        ocupa_mesa(fila, *mesas, pilha, *linhas, *colunas);
         break;
     case 4:
-        remove_grupo_da_fila(fila);
+        printf("Informe o id da mesa da qual deseja retirar pratos: ");
+        scanf("%d", &id_mesa);
+
+        Mesa* mesa = busca_mesa(*mesas, *linhas, *colunas, id_mesa);
+        if(mesa == NULL) 
+        {
+            printf("Mesa não encontrada.\n\n");
+            break;
+        }
+        retira_pratos_da_mesa_sem_desocupar(mesa, pilha);
         break;
     case 5:
-        imprime_pilha_de_pratos(*pilha);
+        remove_grupo_da_fila(fila, true);
         break;
     case 6:
-        imprime_fila_de_espera(*fila);
+        imprime_pilha_de_pratos(*pilha);
         break;
     case 7:
-        imprime_ocupacao_das_mesas(mesas, linhas, colunas);
+        imprime_fila_de_espera(*fila);
         break;
     case 8:
+        imprime_ocupacao_das_mesas(*mesas, *linhas, *colunas);
+        break;
+    case 9:
         return 0;
     }
 
